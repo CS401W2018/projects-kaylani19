a6dfa4580dfee7945cef15bbf8833f938a89be65
Kaylani <itsjustlani19@gmail.com>
2024-11-15 22:30:22 -1000

    Add Form Processing with AJAX files

---
+++ activities/Engagements/FormProcessing/form.js
@@ -0,0 +1,37 @@
+document.getElementById('myForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const first = document.getElementById("firstNameInput").value;
+    const last = document.getElementById("lastNameInput").value;
+    const age = document.getElementById("age").value;
+
+    if(!first || !last){
+        alert("Please provide your full name.");
+        return;
+    }
+
+    if(!age || age < 18){
+        alert("You must be 18 years or older to submit this form.");
+        return;
+    }
+
+    const data ={
+        firstName: first,
+        lastName: last,
+        age: age
+    };
+
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST", "submit.json", true);
+    xhr.setRequestHeader("Content-Type", "application/json;sharset=UTF-8");
+    xhr.onreadystatechange = function () {
+        if(xhr.readyState === 4 && xhr.status === 200) {
+            const response = JSON.parse(xhr.responseText);
+            document.getElementById("message").innerHTML = response.message;
+            document.getElementById("myForm").innerHTML = "";
+        } else if(xhr.readyState === 4){
+            alert('Error submitting form.');
+        }
+    };
+    xhr.send(JSON.stringify(data));
+    console.log(data);
+});
